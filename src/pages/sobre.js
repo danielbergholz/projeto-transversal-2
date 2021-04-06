62ed6080b29ea1d167eeec7cdf253e28ca1ae0d4
lp-mateus <81657778+lp-mateus@users.noreply.github.com>
2021-04-06 00:03:32 -0300

    Criação de página inicial e TopBar
    Criação da identidade visual e Topbar do site.

---
+++ src/pages/sobre.js
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import styles from './../styles/Sobre.module.css';
+import Topbar from './../components/Appbar/topbar.js';
+import { Autorenew } from '@material-ui/icons';
+//import Background from './../../public/finance.jpg';
+
+export default function Sobre() {
+  return (
+      <body className={styles.main}>
+            <Topbar />
+            <div className={styles.center}>  
+                <h1>Projeto de Previsão de Ativos Financeiros</h1>
+                <hr className={styles.line}></hr>           
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+
+                <br></br>
+
+                <Image 
+                    src="/resized-financeiro.png"      
+                    width="704px"
+                    height="650px"                                   
+                />
+
+                <br></br>
+                <br></br>
+
+                <button className={styles.btnhome}>Tutorial</button>
+                <br></br>
+                <br></br>
+                <button className={styles.btnhome}>Solicitar Previsão</button>
+            </div>                     
+      </body>
+
+  
+  )
+}
