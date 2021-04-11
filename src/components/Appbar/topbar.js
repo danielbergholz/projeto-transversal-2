557d351a1aeded17bec1088fca93200722527bd0
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-04-11 18:37:39 -0300

    fix: next links

--- src/components/Appbar/topbar.js
+++ src/components/Appbar/topbar.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import React from 'react'
+import Link from 'next/link'
+import clsx from 'clsx'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import List from '@material-ui/core/List'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -75,20 +76,20 @@ const useStyles = makeStyles((theme) => ({
     }),
     marginRight: 0,
   },
-}));
+}))
 
 export default function PersistentDrawerRight() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const classes = useStyles()
+  const theme = useTheme()
+  const [open, setOpen] = React.useState(false)
 
   const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleDrawerClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   return (
     <div className={classes.root}>
@@ -101,10 +102,11 @@ export default function PersistentDrawerRight() {
         })}
       >
         <Toolbar>
-          
-            <Typography variant="h6" noWrap className={classes.title}>
-              <a href="/">UnB | Previsão de Ativos Financeiros</a>
-            </Typography>
+          <Typography variant="h6" noWrap className={classes.title}>
+            <Link href="/">
+              <a>UnB | Previsão de Ativos Financeiros</a>
+            </Link>
+          </Typography>
 
           <IconButton
             color="inherit"
@@ -121,8 +123,7 @@ export default function PersistentDrawerRight() {
         className={clsx(classes.content, {
           [classes.contentShift]: open,
         })}
-      >
-      </main>
+      ></main>
       <Drawer
         className={classes.drawer}
         variant="persistent"
@@ -134,19 +135,27 @@ export default function PersistentDrawerRight() {
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === 'rtl' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
         <List>
-          {['Home', 'Ativos Financeiro', 'Tutorial', 'Sobre'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>      
+          {['Home', 'Ativos Financeiro', 'Tutorial', 'Sobre'].map(
+            (text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            )
+          )}
+        </List>
       </Drawer>
     </div>
-  );
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,44 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './../styles/Sobre.module.css';
-import Topbar from './../components/Appbar/topbar.js';
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './../styles/Sobre.module.css'
+import Topbar from './../components/Appbar/topbar.js'
 //import Background from './../../public/finance.jpg';
 
 export default function Sobre() {
   return (
-      <body className={styles.main}>
-            <Topbar />
-            <div className={styles.center}>  
-                <h1>Projeto de Previsão de Ativos Financeiros</h1>
-                <hr className={styles.line}></hr>           
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+    <body className={styles.main}>
+      <Topbar />
+      <div className={styles.center}>
+        <h1>Projeto de Previsão de Ativos Financeiros</h1>
+        <hr className={styles.line}></hr>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
 
-                <br></br>
+        <br></br>
 
-                <Image 
-                    src="/resized-financeiro.png"      
-                    width="704px"
-                    height="650px"                                   
-                />
+        <Image src="/resized-financeiro.png" width="704px" height="650px" />
 
-                <br></br>
-                <Link href="/tutorial">
-                    <button className={styles.btnhome}>Tutorial</button>
-                </Link>
-                <br></br>
-                <Link href="/ativo-financeiro">
-                    <button className={styles.btnhome}>Solicitar Previsão</button>
-                </Link>
-                <br></br>
-            </div>                     
-      </body>
+        <br></br>
+        <Link href="/tutorial">
+          <a>
+            <button className={styles.btnhome}>Tutorial</button>
+          </a>
+        </Link>
+        <br></br>
+        <Link href="/ativo-financeiro">
+          <a>
+            <button className={styles.btnhome}>Solicitar Previsão</button>
+          </a>
+        </Link>
+        <br></br>
+      </div>
+    </body>
   )
 }
