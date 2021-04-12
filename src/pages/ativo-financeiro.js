e764c2aa0d84d99e2a10358b768b138602de2f96
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-04-12 10:30:27 -0300

    feat: integrate with news api

--- src/pages/ativo-financeiro.js
+++ src/pages/ativo-financeiro.js
@@ -7,14 +7,17 @@ import styles from '../styles/AtivoFinanceiro.module.css'
 export default function AtivoFinanceiro() {
   const [loading, setLoading] = useState(false)
   const [selectedStock, setSelectedStock] = useState(null)
+  const [news, setNews] = useState([])
 
   const loadGraphAndNews = useCallback(
     async (stock) => {
       setLoading(true)
+      setNews([])
       setSelectedStock(stock)
 
       try {
-        const { data } = await newsApi.get(`/news/petrobras`)
+        const { data } = await newsApi.get(`/news/${stock}`)
+        setNews(data)
         console.log({ data })
       } catch (err) {
         console.log({ err })
@@ -22,7 +25,7 @@ export default function AtivoFinanceiro() {
 
       setLoading(false)
     },
-    [setLoading, setSelectedStock]
+    [setLoading, setSelectedStock, setNews]
   )
 
   return (
@@ -31,7 +34,7 @@ export default function AtivoFinanceiro() {
         <h1>Selecione o ativo financeiro</h1>
         <Button onClick={() => loadGraphAndNews('petrobras')}>Petrobras</Button>
         <Button onClick={() => loadGraphAndNews('ambev')}>Ambev</Button>
-        <Button onClick={() => loadGraphAndNews('itausa')}>Itaúsa</Button>
+        <Button onClick={() => loadGraphAndNews('itau')}>Itaú</Button>
         <Button onClick={() => loadGraphAndNews('cielo')}>Cielo</Button>
       </div>
       <div className={styles.graph}>
@@ -39,13 +42,16 @@ export default function AtivoFinanceiro() {
         {selectedStock && !loading && <h1>Gráfico carregado</h1>}
       </div>
       <div className={styles.news}>
-        {selectedStock && (
-          <>
-            <h2>Notícia 1 sobre {selectedStock}</h2>
-            <h2>Notícia 2 sobre {selectedStock}</h2>
-            <h2>Notícia 3 sobre {selectedStock}</h2>
-          </>
-        )}
+        {selectedStock &&
+          news.length !== 0 &&
+          news.map((item, index) => (
+            <div key={index} className={styles.box}>
+              <a href={item.link} target="_blank">
+                <h2>{item.company}</h2>
+                <p>{item.title}</p>
+              </a>
+            </div>
+          ))}
       </div>
     </div>
   )
