91ae54c471d2850504b92170e6c01e999ab6a11d
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-04-12 10:36:22 -0300

    feat: add navbar to main page

--- src/pages/ativo-financeiro.js
+++ src/pages/ativo-financeiro.js
@@ -3,6 +3,7 @@ import { useState, useCallback } from 'react'
 import { newsApi } from '../services/api'
 import Button from '../components/Button'
 import styles from '../styles/AtivoFinanceiro.module.css'
+import Topbar from './../components/Appbar/topbar.js'
 
 export default function AtivoFinanceiro() {
   const [loading, setLoading] = useState(false)
@@ -29,30 +30,35 @@ export default function AtivoFinanceiro() {
   )
 
   return (
-    <div className={styles.center}>
-      <div className={styles.header}>
-        <h1>Selecione o ativo financeiro</h1>
-        <Button onClick={() => loadGraphAndNews('petrobras')}>Petrobras</Button>
-        <Button onClick={() => loadGraphAndNews('ambev')}>Ambev</Button>
-        <Button onClick={() => loadGraphAndNews('itau')}>Itaú</Button>
-        <Button onClick={() => loadGraphAndNews('cielo')}>Cielo</Button>
+    <>
+      <Topbar />
+      <div className={styles.center}>
+        <div className={styles.header}>
+          <h1>Selecione o ativo financeiro</h1>
+          <Button onClick={() => loadGraphAndNews('petrobras')}>
+            Petrobras
+          </Button>
+          <Button onClick={() => loadGraphAndNews('ambev')}>Ambev</Button>
+          <Button onClick={() => loadGraphAndNews('itau')}>Itaú</Button>
+          <Button onClick={() => loadGraphAndNews('cielo')}>Cielo</Button>
+        </div>
+        <div className={styles.graph}>
+          {selectedStock && loading && <img src="/spinner.png" width="80px" />}
+          {selectedStock && !loading && <h1>Gráfico carregado</h1>}
+        </div>
+        <div className={styles.news}>
+          {selectedStock &&
+            news.length !== 0 &&
+            news.map((item, index) => (
+              <div key={index} className={styles.box}>
+                <a href={item.link} target="_blank">
+                  <h2>{item.company}</h2>
+                  <p>{item.title}</p>
+                </a>
+              </div>
+            ))}
+        </div>
       </div>
-      <div className={styles.graph}>
-        {selectedStock && loading && <img src="/spinner.png" width="80px" />}
-        {selectedStock && !loading && <h1>Gráfico carregado</h1>}
-      </div>
-      <div className={styles.news}>
-        {selectedStock &&
-          news.length !== 0 &&
-          news.map((item, index) => (
-            <div key={index} className={styles.box}>
-              <a href={item.link} target="_blank">
-                <h2>{item.company}</h2>
-                <p>{item.title}</p>
-              </a>
-            </div>
-          ))}
-      </div>
-    </div>
+    </>
   )
 }
